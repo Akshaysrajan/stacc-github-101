a6d1d4df330ac0fcf86bb55f25f814c3f1cbf7ff
Akshaysrajan <akshaysrajanofficial@gmail.com>
2025-09-25 21:42:40 +0530

    V1

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import UserProfile from "./components/UserProfile"
+
+
+const App = () => {
+  return (
+    <>
+    
+    
+    
+
+  <center>  <h1> STACC GITHUB 101 </h1> 
+    
+    
+     <div  >
+    
+      
+      
+       <UserProfile name="Akshay" image= "/images/akshay.jpg">
+        Msc AI 2nd year           
+         </UserProfile>     
+           
+{/* use the above template and create add your data */}
+
+
+     <UserProfile name="Akshay" image= "/images/akshay.jpg">
+        Msc AI 2nd year           
+         </UserProfile>     
+         
+         
+          <UserProfile name="Akshay" image= "/images/akshay.jpg">
+        Msc AI 2nd year           
+         </UserProfile>      
+         
+         <UserProfile name="Akshay" image= "/images/akshay.jpg">
+        Msc AI 2nd year           
+         </UserProfile> 
+ 
+
+
+    </div>
+</center>
+    
+    
+  </>
+  );
+};export default App;
